d2afdaa767f81010de84208fc4269af9ec7d00fa
SPIAlikhanian01 <alikhanian.mohammad@gmail.com>
2025-04-04 16:38:04 +0430

    20250404-1637

---
+++ Infrastructure/Repositories/CountryRepository.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class CountryRepository : ICountryRepository
+    {
+        private readonly YourDbContext _context;
+
+        public CountryRepository(YourDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Country>> GetAllAsync()
+            => await _context.Countries.AsNoTracking().ToListAsync();
+
+        public async Task<Country> GetByIdAsync(int id)
+            => await _context.Countries.FindAsync(id);
+
+        public async Task AddAsync(Country country)
+            => await _context.Countries.AddAsync(country);
+
+        public void Update(Country country)
+            => _context.Countries.Update(country);
+
+        public void Remove(Country country)
+            => _context.Countries.Remove(country);
+
+        public async Task<bool> ExistsAsync(int id)
+            => await _context.Countries.AnyAsync(c => c.Id == id);
+    }
+
+}
